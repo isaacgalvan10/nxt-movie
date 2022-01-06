d806e7e6682bc136f5b6ad1e6306f407dfd0d812
Isaac Galvan <isaacgdev@gmail.com>
2022-01-05 18:08:28 -0600

    Merge branch 'main' of github.com:isaacgalvan10/nxt-movie into main

--- assets/results.js
+++ assets/results.js
@@ -4,36 +4,27 @@ var movieTitle = document.querySelector("#title");
 var movielist = [];
 
 
-// Cuando se abra la pagina, cargar la lista de watchlist desde el localstorage
-
-// function init
-// Obtener info de localstorage
-//hacer render de lista
-// POr cada elemento del local storage, crear un elemento li
-
-
-
 function renderTodos() {
-    watchList.innerHTML = ""
+    watchList.innerHTML = "";
     for (var i = 0 ; i < movielist.length; i++) {
 
     var li = document.createElement("li");
-    li.setAttribute("style","display:flex; justify-content: flex-start; align-items:center; background: #04293A; position: relative; padding:1px")
-    li.classList = "box"
+    li.setAttribute("style","display:flex; justify-content: flex-start; align-items:center; background: #04293A; position: relative; padding: 0; margin-top: 10px;");
+    li.classList = "box";
 
     var pos = document.createElement("img");
-    pos.setAttribute("style","width:50px; height:auto")
+    pos.setAttribute("style","width:60px; height:auto; border-radius: 6px 0 0 6px;")
     pos.setAttribute("src", movielist[i].moviePoster)
 
     var tex = document.createElement("a")
     tex.textContent = movielist[i].movieTitle;
     tex.setAttribute("href",movielist[i].movieUrl)
-    tex.setAttribute("style", "color:white; font-weight: bold; padding-left: 5px")
+    tex.setAttribute("style", "color:white; font-weight: 600; padding-left: 10px; font-size: 18px;")
 
     var button = document.createElement("button");
     button.textContent = "X";
     button.setAttribute("data-index", i)
-    button.setAttribute("style", "padding: 13px 8px; background:#ECB365; color:black; font-weight: bold; position: absolute; top: 0; right: 0; border: none; height: 25px;")
+    button.setAttribute("style", "padding: 13px 8px; background:#ECB365; color:black; font-weight: bold; position: absolute; top: 0; right: 0; border: none; height: 25px; border-radius: 6px;")
     button.classList = "button is-small"
 
     li.append(pos,tex,button);
